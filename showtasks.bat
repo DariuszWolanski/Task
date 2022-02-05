6cf50b57e9e3694f0def0aec68f57938d439281a
Dariusz Wolanski <dariusz.wolanski@poczta.onet.pl>
2022-02-05 16:42:40 +0100

    Modul 21.4

---
+++ showtasks.bat
@@ -0,0 +1,22 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto getTasks
+echo.
+echo Cannot run crud.war
+goto fail
+
+:getTasks
+call http://localhost:8080/crud/v1/task
+if "%ERRORLEVEL%" == "0" goto end
+echo Cannot open browser
+
+:end
+echo.
+echo Successfull
+
+:fail
+echo.
+echo Unsuccessful
+
+:end
+echo.
+echo Showed tasks successfully
